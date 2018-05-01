c64ee93d33c004483baedee8621f4046bac7974b
amanbur <adrianmanso20_21@hotmail.com>
2018-05-01 19:30:49 +0200

    01/05/2018 II

--- scripts/tema2/01-summarize.R
+++ scripts/tema2/01-summarize.R
@@ -18,3 +18,25 @@ install.packages(c("modeest", "raster", "moments"))
 library(modeest) #moda
 library(raster) #quantiles, cv
 library(moments)
+
+
+X = data$mpg
+
+#### Medidas de Centralización
+mean(X) #sum(X)/length(X)
+median(X)
+mfv(X)
+quantile(X)
+
+#### Medidas de Dispersión
+var(X)
+sd(X)
+cv(X) #sd(X)/mean(X)
+
+#### Medidas de asimetría
+skewness(X)
+moments::kurtosis(X)
+
+par(mfrow = c(1,1))
+hist(X)
+
